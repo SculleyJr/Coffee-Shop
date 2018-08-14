d58c496809e686c09bd7c58c16e67ee402e1d815
Sean Sculley <doneotreply@github.com>
2018-08-14 08:30:40 -0400

    first commit

---
+++ BootcampBeanery/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BootcampBeanery.Models;
+
+namespace BootcampBeanery.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult New()
+        {
+
+           
+            ViewBag.Message = "Enter your information below to register";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult New(Customer cust)
+        {
+            return RedirectToAction("ThankYou", cust);
+        }
+
+        public ActionResult ThankYou(Customer model)
+        {
+            return View(model);
+        }
+
+    
+
+    }
+}
---
+++ BootcampBeanery/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.EnterpriseServices.Internal;
+using System.Linq;
+using System.Web;
+
+namespace BootcampBeanery.Models
+{
+    public class Customer
+    {
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+            
+    
+
+       
+    }
+}
